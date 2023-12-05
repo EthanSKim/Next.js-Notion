bde88c9e7fe4bdec13fc1e8f9d2275ced2df197f
Ethan Kim <ethankim9091@gmail.com>
2023-12-04 18:49:21 -0600

    implemented auth using convex

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,9 @@
+const DocumentsPage = () => {
+    return ( 
+        <div>
+            yo
+        </div>
+     );
+}
+ 
+export default DocumentsPage;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers:[
+        {
+            domain: "https://liked-dingo-91.clerk.accounts.dev",
+            applicationID: "convex"
+        }
+    ]
+}
